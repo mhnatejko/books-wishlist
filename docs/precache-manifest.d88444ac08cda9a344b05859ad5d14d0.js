ebef41df6901956221ed0541201516d19c0bba7d
Maksymilian Hnatejko <maksymilian.hnatejko@gmail.com>
2018-11-21 21:21:46 +0100

    add docs

---
+++ docs/precache-manifest.d88444ac08cda9a344b05859ad5d14d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a383e493594eb1e92a3f91dec81c0a3",
+    "url": "/books-wishlist/static/media/photo-1515125520141-3e3b67bc0a88.9a383e49.jpg"
+  },
+  {
+    "revision": "ce0659942ecdbb86ccaf8c06d4ac1d35",
+    "url": "/books-wishlist/static/media/logo-fe2ef7d630d5f23daf46230e8592cd99.ce065994.png"
+  },
+  {
+    "revision": "4e6305179128deffb3ba",
+    "url": "/books-wishlist/static/js/runtime~main.4e630517.js"
+  },
+  {
+    "revision": "5dd671161032efe0dda4",
+    "url": "/books-wishlist/static/js/main.5dd67116.chunk.js"
+  },
+  {
+    "revision": "17fb4ccabd0d1abe5169",
+    "url": "/books-wishlist/static/js/1.17fb4cca.chunk.js"
+  },
+  {
+    "revision": "5dd671161032efe0dda4",
+    "url": "/books-wishlist/static/css/main.205b710f.chunk.css"
+  },
+  {
+    "revision": "17fb4ccabd0d1abe5169",
+    "url": "/books-wishlist/static/css/1.4fcf4be6.chunk.css"
+  },
+  {
+    "revision": "b6dd7e18271d408a1dfef6b7b401cb54",
+    "url": "/books-wishlist/index.html"
+  }
+];
